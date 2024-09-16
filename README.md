6dc59fe356458711bf84ab26dd7acf46fac6e7d9
Andrii Oriekhov <andriyorehov@gmail.com>
2024-09-16 12:18:27 +0300

    add export to csv

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# monobank-airtable
+# monobank-export
 
 ### Config
 
@@ -12,3 +12,23 @@ AIRTABLE_BASE="******"
 ```
 
 For mcc used this data [Oleksios/Merchant-Category-Codes: MCC codes dataset in Ukrainian, English and Russian (with groups and without groups)](https://github.com/Oleksios/Merchant-Category-Codes)
+
+## Examples
+
+export to airtable
+
+```sh
+tsx src/index.ts
+```
+
+export to csv file
+
+```sh
+tsx src/monobank-csv.ts
+```
+
+## API Doc
+
+[Monobank open API](https://api.monobank.ua/docs/index.html#tag/Kliyentski-personalni-dani/paths/~1personal~1webhook/post)
+
+[API. Особистий кабінет](https://api.monobank.ua/index.html)
---
+++ src/monobank-csv.ts
@@ -0,0 +1,64 @@
+import { createWriteStream } from 'node:fs';
+
+import { stringify } from 'csv-stringify';
+import { format } from 'date-fns';
+import { MonobankApi, Statement } from 'monobank-api';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+type StatementsCsv = {
+  id: string;
+  time: string;
+  description: string;
+  mcc: number;
+  amount: number;
+};
+
+const transformStatements = (statements: Statement[]) => {
+  return statements
+    .filter((row) => row.amount < 0)
+    .map((row) => {
+      const date = new Date(row.time * 1000);
+      return {
+        id: row.id,
+        time: format(date, 'yyyy-MM-dd'),
+        description: row.description,
+        mcc: row.mcc,
+        amount: Math.round(row.amount / 100),
+      };
+    });
+};
+
+const writeCsv = (filename: string, statements: StatementsCsv[]) => {
+  const writableStream = createWriteStream(filename);
+
+  const columns = ['id', 'time', 'description', 'mcc', 'amount'];
+
+  const stringifier = stringify({ header: true, columns: columns });
+
+  statements.forEach((row) => {
+    stringifier.write(row);
+  });
+
+  stringifier.pipe(writableStream);
+};
+
+(async () => {
+  const monobankApi = new MonobankApi(process.env.MONO_TOKEN || '');
+  const account = process.env.MONO_ACCOUNT || '';
+
+  const startDate = new Date('2024.9.1');
+  const endDate = new Date('2024.9.16');
+
+  const statements = await monobankApi.getAllStatements({
+    account,
+    from: startDate,
+    to: endDate,
+  });
+
+  const newStatements = transformStatements(statements);
+
+  const filename = 'example.csv';
+  writeCsv(filename, newStatements);
+})();
